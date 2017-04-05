5533dc4233c044f894feb1217fec6f8e2640e1df
darkblackart <zebrowski27200@student.pwsztar.edu.pl>
2017-04-05 19:07:03 +0200

    passing

--- app/app.js
+++ app/app.js
@@ -1,7 +1,24 @@
 function findMin(array)
 {
-
+    var result = array[0];
+    if (isNaN(result)) {
+        return false;
+    }
+    var dlu = array.length;
+    for (var i = 1; i<dlu;i++){
+        if (isNaN(array[i])) {
+            return false;
+        }
+        else{
+            if (result > array[i]) {
+                result = array[i];
+            }
+        }
+    }
+    return result;
 }
 module.exports = {
     findMin: findMin
 };
+
+
