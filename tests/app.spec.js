c5e4fd5f7e5cdbe8b8de3b8a98e1fe96b9350200
darkblackart <zebrowski27200@student.pwsztar.edu.pl>
2017-04-05 19:17:19 +0200

    desciption

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
-
-it('should return false when parameters arent numbers', function(){
+describe('when paraments arent numbers', function())
+it('should return false ', function(){
     expect(app.findMax([1, 2, 3, 'a'])).to.eql(false);
 });
-
+describe('when all paraments are correct',function())
 it('should return min value', function () {
     expect(app.findMax([1, 2, 3])).to.eql(3);
 });
