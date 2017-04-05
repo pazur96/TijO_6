2e9380f9e124bf68ac6451823fe764c94ea1edad
Kamil Bachara <szpala27189@pwsztar.edu.pl>
2017-04-05 13:06:05 +0200

    add function findMax

--- app/app.js
+++ app/app.js
@@ -1,6 +1,21 @@
 function findMax(array)
 {
-
+    var result = array[0];
+    if (isNaN(result)) {
+        return false;
+    }
+    var dlu = array.length;
+    for (var i = 1; i<dlu;i++){
+        if (isNaN(array[i])) {
+            return false;
+        }
+        else{
+            if (result < array[i]) {
+                result = array[i];
+            }
+        }
+    }
+    return result;
 }
 module.exports = {
     findMax: findMax
